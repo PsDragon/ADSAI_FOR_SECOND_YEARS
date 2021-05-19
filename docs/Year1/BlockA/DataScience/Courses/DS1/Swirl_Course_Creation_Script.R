599548b5dac3a47fca0e50afc657ea6b4e10bba5
BramHeijligers <83017828+BramHeijligers@users.noreply.github.com>
2021-05-19 17:52:56 +0200

    Revert "Revert "Iterated on workshop 1 - intro to R""
    This reverts commit 8aebef5d7765bc5cd62fa3ce16b4b10a8bd33501.

--- docs/Year1/BlockA/DataScience/Courses/DS1/Swirl_Course_Creation_Script.R
+++ docs/Year1/BlockA/DataScience/Courses/DS1/Swirl_Course_Creation_Script.R
@@ -3,9 +3,10 @@
 #Resources
 
 # Load packages
-library(swirlify)
+require(swirlify)
+require(swirl)
 
 # Create a lesson and repo
-setwd("C:/Users/bramh/Documents/GitHub/AAI-DM/docs/Year1/BlockA/DataScience/Courses")
-new_lesson("Workshop_1_The_World_in_Data", "DS1")
+setwd("C:/Users/bramh/Documents/GitHub/AAI-DM/docs/Year1/BlockA/DataScience/Courses/DS1/Day1")
+new_lesson("Introduction to R-studio", "Workshop 1")
 

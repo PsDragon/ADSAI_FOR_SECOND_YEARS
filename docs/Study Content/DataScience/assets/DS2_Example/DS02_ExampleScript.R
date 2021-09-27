e5f14848bc6ba527cbddf772da16873a0d41a9d6
BramHeijligers <83017828+BramHeijligers@users.noreply.github.com>
2021-09-27 17:59:48 +0200

    Update DS02_ExampleScript.R

--- docs/Study Content/DataScience/assets/DS2_Example/DS02_ExampleScript.R
+++ docs/Study Content/DataScience/assets/DS2_Example/DS02_ExampleScript.R
@@ -4,21 +4,21 @@
 library(dplyr)
 
 # Get the 'railways-passengers-carried-passenger-km.csv' file into a folder of your liking, probably the downloads folder
-path <- "YourPathHere"
-file <-  "railways-passengers-carried-passenger-km.csv"
+path <- "YourPathHere" #So your 'path' is the location where you saved your file and where R is going to look for it
+file <-  "railways-passengers-carried-passenger-km.csv" # name of the file
 
 # Import the dataset from the downloads folder
-df <- read.csv(paste0(path,file))
+df <- read.csv(paste0(path,file)) # Combine the name and path of the file to find the file and load it in as a data.frame called 'df'
 
 # Change collumn name
 colnames(df) # What are the collumn names?
 colnames(df)[4] <- "Passengers per Million Kilometers" # Change just one variable name
-colnames(df)
+colnames(df)  # Did it work?
 colnames(df) <- c("country",  "code", "year","pmk") # Change all of them, best practise is to use only small letters and short words, abrevations or acronyms
-colnames(df)
+colnames(df) # Did it work?
 
-# Delete the code collumn
-df <- df[,-2]
+# Delete the 'code'  collumn
+df <- df[,-2] # I don't need that for now, also you get an example of how to delete a collumn
 
 
 # Get a specific country by manually selecting rows
@@ -31,9 +31,9 @@ plot(df_alb$year, df_alb$pmk, type = "s") # same plot but with steps
 ?plot # for more options
 
 # Final example plot
-plot(df_alb$year, df_alb$pmk, type = "b", main = "Railway business in Albania", xlab = "Year", ylab = "Passengers per Million Kilometers", col = "black") # same plot but with a line and dots
+plot(df_alb$year, df_alb$pmk, type = "b", main = "Railway business in Albania", xlab = "Year", ylab = "Passengers per Million Kilometers", col = "black") # open ?plot and look at the documentation, do you understand what I did here?
 "Something like that could go in your poster! :D"
-summary(df_alb)
+summary(df_alb) # Generate descriptive statistics
 
 
 # Get a specific year 
@@ -48,13 +48,17 @@ hist(df_2017$pmk) # Create preliminary histogram
 "There are other descriptive stats packages available, I used two good ones!"
 
 # More descriptive statistics
-install.packages("psych")
-library(psych)
-describe(df_2017$pmk)
-kurtosi(df_2017$pmk)
+install.packages("psych") # Install the package
+library(psych) # Load the package
+describe(df_2017$pmk) # Generate descriptive statistics
+kurtosi(df_2017$pmk) # You can also generate specific descriptive stats like the kurtosis
+"Check out the psych documentation for more cool functions!"
+
 detach("package:psych", unload = TRUE)
 
 
-install.packages("Hmisc")
-library(Hmisc)
-describe(df_2017$pmk)
+install.packages("Hmisc") # Install the package
+library(Hmisc) # Load the package
+describe(df_2017$pmk) # Generate descriptive statistics
+"Check out the Hmisc documentation for more cool functions!"
+
